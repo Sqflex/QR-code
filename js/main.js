0e8012e6307540c2f0d5db910baabd962899bc51
SQflex <32853595+Sqflex@users.noreply.github.com>
2021-01-30 04:46:29 +0300

    fix: smoothscroll problem

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function(){
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    var scroll = new SmoothScroll('.navbar a[href*="#"]', {
+        speed: 800,
+        offset: 100
+    }); 
     particlesJS.load('particles-js', 'assets/particles/particles.json', function () {
         console.log('callback - particles.js config loaded');
     }); 
 })
 
-var scroll = new SmoothScroll('.navbar a[href*="#"]', {
-    speed: 800,
-    offset: 100
-}); 
+
